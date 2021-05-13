19b51fa353d163f080ba7dc925e3390e6483889e
WDRK <lllrrrlrssr@gmail.com>
2021-05-13 21:16:14 +0900

    입력창에서 엔터키를 누르면 keypress 이벤트로 감지해서 메시지를 보내도록 설정.

--- src/js/chat.js
+++ src/js/chat.js
@@ -7,6 +7,23 @@ const nickname = document.querySelector('#nickname');
 const chatList = document.querySelector('.chatting-list');
 const chatInput = document.querySelector('.chatting-input');
 const sendButton = document.querySelector('.send-button');
+const displayContainer = document.querySelector('.display-container');
+
+function send() {
+  const param = {
+    name: nickname.value,
+    msg: chatInput.value,
+  };
+  // emit('채널이름', '내용')은 메시지를 보내는 방법
+  socket.emit('chatting', param);
+}
+chatInput.addEventListener('keypress', (event) => {
+  /* chatInput 창에서 엔터를 감지해서 동작하는 설정 */
+  if (event.keyCode === 13) {
+    send();
+    chatInput.value = '';
+  }
+});
 
 function LiModel(name, msg, time) {
   this._name = name;
@@ -31,14 +48,7 @@ function LiModel(name, msg, time) {
     chatList.appendChild(li);
   };
 }
-sendButton.addEventListener('click', () => {
-  const param = {
-    name: nickname.value,
-    msg: chatInput.value,
-  };
-  // emit('채널이름', '내용')은 메시지를 보내는 방법
-  socket.emit('chatting', param);
-});
+sendButton.addEventListener('click', send);
 
 // 들어오는 메시지를 받는 코드
 socket.on('chatting', (data) => {
@@ -46,4 +56,8 @@ socket.on('chatting', (data) => {
   const { name, msg, time } = data;
   const item = new LiModel(name, msg, time);
   item._makeLi();
+  displayContainer.scrollTo(
+    0,
+    displayContainer.scrollHeight
+  ); /* 채팅이 쌓일때 스크롤이 자동으로 내려가는 설정 */
 });
