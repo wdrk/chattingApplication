fd3715b6ea454911bfb4daae4947817629b86333
WDRK <lllrrrlrssr@gmail.com>
2021-05-13 15:06:19 +0900

    html 대화상자에 입력한 값을 chat.js에서 읽어서 app.js로 보내는 기능 추가

--- src/js/chat.js
+++ src/js/chat.js
@@ -3,8 +3,19 @@
 // socket 변수에 client socket.io를 담는다
 const socket = io();
 
-// emit('채널이름', '내용')은 메시지를 강제로 보내는 방법
-socket.emit('chatting', 'from front');
+const nickname = document.querySelector('#nickname');
+const chatList = document.querySelector('.chatting-list');
+const chatInput = document.querySelector('.chatting-input');
+const sendButton = document.querySelector('.send-button');
+
+sendButton.addEventListener('click', () => {
+  const param = {
+    name: nickname.value,
+    msg: chatInput.value,
+  };
+  // emit('채널이름', '내용')은 메시지를 보내는 방법
+  socket.emit('chatting', param);
+});
 
 // 들어오는 메시지를 받는 코드
 socket.on('chatting', (data) => {
