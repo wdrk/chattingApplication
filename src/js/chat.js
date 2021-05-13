0b8e78cd9872e2494c5580cebc250e3bf062aedc
WDRK <lllrrrlrssr@gmail.com>
2021-05-13 20:46:47 +0900

    대화명을 식별해서 나와 상대방의 메시지에 각각 다른 CSS를 적용. 대화내용을 appendChild(li)로 계속 생성함

--- src/js/chat.js
+++ src/js/chat.js
@@ -8,6 +8,29 @@ const chatList = document.querySelector('.chatting-list');
 const chatInput = document.querySelector('.chatting-input');
 const sendButton = document.querySelector('.send-button');
 
+function LiModel(name, msg, time) {
+  this._name = name;
+  this._msg = msg;
+  this._time = time;
+
+  //
+  this._makeLi = () => {
+    const li = document.createElement('li');
+    li.classList.add(nickname.value === this._name ? 'sent' : 'received');
+    const dom = `<span class="profile">
+                  <span class="user">${this._name}</span>
+                  <img
+                    class="image"
+                    src="https://placeimg.com/50/50/any"
+                    alt="any"
+                  />
+                 </span>
+                 <span class="message">${this._msg}</span>
+                 <span class="time">${this._time}</span>`;
+    li.innerHTML = dom;
+    chatList.appendChild(li);
+  };
+}
 sendButton.addEventListener('click', () => {
   const param = {
     name: nickname.value,
@@ -19,8 +42,7 @@ sendButton.addEventListener('click', () => {
 
 // 들어오는 메시지를 받는 코드
 socket.on('chatting', (data) => {
-  const li = document.createElement('li');
-  li.innerText = `${data.name}님이 - ${data.msg}`;
-  chatList.appendChild(li);
+  const { name, msg, time } = data;
+  const item = new LiModel(name, msg, time);
+  item._makeLi();
 });
-console.log(socket);
